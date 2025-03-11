3e2ab69277dd97a31748f858fb34c96474a10fe7
Spencer Davis <spencerdavis226@gmail.com>
2025-03-11 11:22:30 -0400

    Added User.js schema

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Pre-save hook to hash passwords before saving a user document
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to compare given password with hashed password
+UserSchema.methods.comparePassword = async function (pw) {
+  return bcrypt.compare(pw, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
